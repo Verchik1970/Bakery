a8fa87151626613609651c39cdcdbccc601be1ff
Verchik1970 <Verchik1970@gmail.com>
2023-07-03 12:33:38 +0300

    New commit (don't work)

---
+++ src/Bakery.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bakery {
+   /* private ArrayList addToBakery;
+    public  Bakery (ArrayList addToBakery){
+        this.addToBakery = addToBakery;
+
+    }*/
+    public static void main(String[] args) {
+        String nameBakery;
+        String art = "rice"; //наименование артикля
+        int qwt = 6; //количество позиций артикля
+
+        List<Bakery> bakery = new ArrayList<>();//создаем список бакалеи
+
+        // добавляем в бакалею артикли
+        public void addToBakery() {
+
+            for (int i = 0; i < qwt; i++) {
+                bakery.add(art);// добавляем строку в список
+                System.out.println(nameBakery + bakery);
+            }
+
+        }
+
+
+    }
+}
+
+
+
+
